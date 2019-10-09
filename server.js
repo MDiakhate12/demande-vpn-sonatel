487a813a71141970acadd0df3e38ea802f2d2cb3
mdiakhate12 <mdiakhate1297@gmail.com>
2019-10-09 09:39:44 +0000

    Modifying servers.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 //Install express server
-const express = require('express');
-const path = require('path');
-var bodyParser = require("body-parser");
+import express, { static } from 'express';
+import { join } from 'path';
+import { json } from "body-parser";
 
 
 const app = express();
-app.use(bodyParser.json());
+app.use(json());
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/Frontend'));
+app.use(static(__dirname + '/dist/Frontend'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/Frontend/index.html'));
+  res.sendFile(join(__dirname + '/dist/Frontend/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
